b954852886c6808d14bd54d8c1d4005c761d582f
Urinboy Tursunboyev <51972400+urinboy@users.noreply.github.com>
2023-03-30 12:08:48 +0500

    deploy sysdc.uz hosting

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware(['auth', 'verified'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
